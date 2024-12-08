6fc96f50a3c4eeeab4437dc7236c502e415d9df4
eviolspirid777 <eviolspirid@gmail.com>
2024-12-08 16:34:19 +0300

    Добавлены TODO, необходимые для деплоя

--- src/app/(main_app)/page.tsx
+++ src/app/(main_app)/page.tsx
@@ -11,6 +11,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const Home = () => {
   const queryClient = new QueryClient();
 
+  //TODO: Нужно сделать loadingPage
+  //TODO: Нужно сделать страницу ошибки
+  //TODO: сделать вариант навигации при выборе категории в header
+  //TODO: Реализовать поиск на декстопной версии и мобильной
+  //TODO: Сделать возможность добавления товара в корзину
+  //TODO: Сделать возможность выбора options у товара
+
+  //TODO: Сделать возможность загрузки фоток в слайдер(ну это уже необязательно)
+  //TODO: Сделать возможность добавления/удаления брендов в админке
+  //TODO: Добавить поиск товаров в таблицу редактирования товаров
+  //TODO: Добавить выборку options у товаров
+  //TODO: Добавить возможность добавления options у товаров
+
+  //THEN READY TO DEPLOY!!!
   return (
     <>
       <QueryClientProvider client={queryClient}>
