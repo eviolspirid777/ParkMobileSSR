832cd9d17c73195bae11bbfe6fe9ab18c74106a1
Kiruha <93467283+eviolspirid777@users.noreply.github.com>
2024-12-09 12:11:38 +0300

    Fixes

---
+++ src/app/(main_app)/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+      <div style={{
+        width:"100vw",
+        height:"100vh",
+        backgroundColor: "black"
+      }}>
+          <img src="/images/ErrorImages/Cow.webp"/>
+      </div>
+  );
+}
