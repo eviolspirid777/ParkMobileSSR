391f337d4ede60da2bf053b52bd03a66ea6af61a
eviolspirid777 <eviolspirid@gmail.com>
2024-12-17 21:56:12 +0300

    Добавлен клик на категорию в слайдере

--- src/Components/Tiles/TilesMobile/TilesMobile.tsx
+++ src/Components/Tiles/TilesMobile/TilesMobile.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,52 +9,62 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import styles from "./TilesMobile.module.scss";
+import { useAtom } from "jotai";
+import { categoryAtom } from "@/Store/FiltersStore";
+import { animateScroll as scroll } from "react-scroll";
 
 export const TilesMobile = () => {
   const [isClient, setIsClient] = useState(false);
+  const [, setCategories] = useAtom(categoryAtom);
+
+  const handleCategory = (category: string) => {
+    scroll.scrollTo(2552, {
+      duration: 50,
+      smooth: true,
+    });
+
+    setCategories(category);
+  };
+
   const tilesItems = [
     {
       image: "images/TilesImages/AppleWatchTile/appleWatchTileReviewed.png",
       header: "Apple Watch",
       text: "Умнее. Ярче. Могущественнее",
       color: "white",
-      action: () => {}
+      action: () => handleCategory("Watch"),
     },
     {
       image: "images/TilesImages/MacBookTile/macBookReviewed.png",
       header: "MacBook Pro",
       text: "Сногсшибательный. Вскружит голову.",
       color: "black",
-      action: () => {}
+      action: () => handleCategory("Mac"),
     },
     {
       image: "images/TilesImages/AirpodsTile/PodsTileReviewed.png",
       header: "AirPods Pro",
       text: "Никаких проводов. Только магия звука.",
       color: "white",
-      action: () => {}
+      action: () => handleCategory("Airpods"),
     },
     {
       image: "images/TilesImages/IpadTile/ipadTileReviewed.png",
       header: "iPad",
       text: "Твой следующий компьютер - не компьютер.",
       color: "black",
-      action: () => {}
+      action: () => handleCategory("iPad"),
     },
-  ]
+  ];
 
   useEffect(() => {
     setIsClient(true);
-  }, [])
+  }, []);
 
   return (
     <Swiper
-      modules={
-        isClient ? [Navigation, Pagination, Autoplay] : [Autoplay]
-      }
-      pagination={
-        isClient ? undefined : { clickable: true }
-      }
+      modules={isClient ? [Navigation, Pagination, Autoplay] : [Autoplay]}
+      pagination={isClient ? undefined : { clickable: true }}
       navigation={isClient ? true : false}
       autoplay={{
         delay: 5000,
@@ -66,30 +76,30 @@ export const TilesMobile = () => {
         paddingTop: "3%",
       }}
     >
-      {
-        tilesItems.map(el => (
-          <SwiperSlide
-            key={el.header}
+      {tilesItems.map((el) => (
+        <SwiperSlide key={el.header}>
+          <div
+            className={styles["popular-item-slide"]}
+            style={{
+              backgroundImage: `url(${el.image})`,
+              color: `${el.color}`,
+            }}
           >
-            <div
-              className={styles["popular-item-slide"]}
-              style={{
-                backgroundImage: `url(${el.image})`,
-                color: `${el.color}`
-              }}
-            >
-              <div className={styles["popular-item-slide-block"]}>
-                <h2>{el.header}</h2>
-                <span>{el.text}</span>
-                <div className={styles["button-block"]}>
-                  <button data-button="купить">Купить</button>
-                  <button data-button="подробнее">Подробнее</button>
-                </div>
+            <div className={styles["popular-item-slide-block"]}>
+              <h2>{el.header}</h2>
+              <span>{el.text}</span>
+              <div className={styles["button-block"]}>
+                <button data-button="купить" onClick={el.action}>
+                  Купить
+                </button>
+                <button data-button="подробнее" onClick={el.action}>
+                  Подробнее
+                </button>
               </div>
             </div>
-          </SwiperSlide>
-        ))
-      }
+          </div>
+        </SwiperSlide>
+      ))}
     </Swiper>
-  )
-}
+  );
+};
